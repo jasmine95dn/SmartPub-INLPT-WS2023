6322d21d8c82307707ff6ce2214f843355449376
jasmine95dn <jasmine.1995dn@gmail.com>
2024-03-02 18:37:05 +0100

    [Nguyen] Docs retrieval from the database

---
+++ smartpub_app/model/db_retriever.py
@@ -0,0 +1,48 @@
+import numpy as np
+import torch
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from pinecone import Pinecone
+from langchain.vectorstores import Pinecone as pc
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+
+class DBRetriever:
+
+    def __init__(self, api_key:str="PINECONE_API_KEY_REDACTED", index_name:str="smartpub", namespace:str="assignment_embedding", 
+                    model_name:str='sentence-transformers/all-MiniLM-L6-v2', batch_size=32):
+        #If system supports cude, use it for gpu execution
+        if torch.cuda.is_available(): 
+            device = 'cuda:0'
+        else: 
+            device = 'cpu'
+            print("You are working on CPU. This may lead to long computation times.")
+        # Initialize Pinecone client
+        pinecone = Pinecone(api_key=api_key) 
+
+        # Initialize the existing index
+        self.index = pinecone.Index(name=index_name)
+
+        #Using pre-trained STS model
+        self.sts_model = HuggingFaceEmbeddings(
+                model_name=model_name,
+                model_kwargs={'device': device},
+                encode_kwargs={'device': device, 'batch_size': batch_size}
+            )
+
+    def getTopSimilarDocs(self, question:str="Tell me about Schizophrenia?", num_docs:int=5):
+
+        question_embedding = self.sts_model.embed_documents([question])[0]
+        
+        # Calculate STS between question and each document
+        #question_embedding = sts_model.embed_documents([question])[0]
+        
+        self.vectorstore_db = pc(self.index, self.sts_model.embed_query, 'relations')
+        # Query the index to find similar documents
+        
+
+        query_results = self.vectorstore_db.similarity_search(query=question, k=num_docs)
+        # Extract the IDs or indices of the top similar documents
+        
+
+        return 
