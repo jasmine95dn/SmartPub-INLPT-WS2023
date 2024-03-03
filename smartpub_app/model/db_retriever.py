9a1ecf1ceaf81183df33bdaab1c2915124a84d98
jasmine95dn <jasmine.1995dn@gmail.com>
2024-03-03 06:03:01 +0100

    [Nguyen] update retriever

--- smartpub_app/model/db_retriever.py
+++ smartpub_app/model/db_retriever.py
@@ -1,48 +1,37 @@
-import numpy as np
-import torch
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 import pinecone
 from langchain.vectorstores import Pinecone 
-from dotenv import load_dotenv, find_dotenv
-_ = load_dotenv(find_dotenv())
 
 
 class DBRetriever:
 
-    def __init__(self, api_key:str="PINECONE_API_KEY_REDACTED", index_name:str="smartpub", namespace:str="assignment_embedding", 
-                    model_name:str='sentence-transformers/all-MiniLM-L6-v2', batch_size=32):
-        #If system supports cude, use it for gpu execution
-        if torch.cuda.is_available(): 
-            device = 'cuda:0'
-        else: 
-            device = 'cpu'
-            print("You are working on CPU. This may lead to long computation times.")
+    def __init__(self, api_key:str, index_name:str="smartpub", 
+                    model_name:str='sentence-transformers/all-MiniLM-L6-v2', batch_size=32, device=-1):
+
+        """
+        This class uses a vectorstore as a retriever to be part of the QA pipeline to retrieve the most relevant documents
+        Default model is the SentenceTransformer Mini Llama
+
+        :param str api_key: The API key for accessing the service.
+        :param str index_name: The name of the index to be used (default is "smartpub").
+        :param str model_name: The name of the pre-trained model to be used (default is 'sentence-transformers/all-MiniLM-L6-v2').
+        :param int batch_size: The batch size for processing data (default is 32).
+        :param int device: The device to be used for processing (-1 for CPU, default is -1).
+
+        
+        """
+
         # Initialize Pinecone client
         pc = pinecone.Pinecone(api_key=api_key) 
 
         # Initialize the existing index
         self.index = pc.Index(name=index_name)
 
-        #Using pre-trained STS model
-        self.sts_model = HuggingFaceEmbeddings(
+    
+        self.embed_model = HuggingFaceEmbeddings(
                 model_name=model_name,
                 model_kwargs={'device': device},
                 encode_kwargs={'device': device, 'batch_size': batch_size}
             )
+        self.vectorstore_db = Pinecone(self.index, self.embed_model.embed_query, 'relations')
 
-    def getTopSimilarDocs(self, question:str="Tell me about Schizophrenia?", num_docs:int=5):
-
-        question_embedding = self.sts_model.embed_documents([question])[0]
-        
-        # Calculate STS between question and each document
-        #question_embedding = sts_model.embed_documents([question])[0]
-        
-        self.vectorstore_db = Pinecone(self.index, self.sts_model.embed_query, 'relations')
-        # Query the index to find similar documents
-        
-
-        query_results = self.vectorstore_db.similarity_search(query=question, k=num_docs)
-        # Extract the IDs or indices of the top similar documents
-        
-
-        return 
